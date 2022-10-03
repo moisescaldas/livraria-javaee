8a3e1fd299da53d7791082b471809ad7b454c9f2
Moises Costa Caldas <sesiom.br@gmail.com>
2022-10-03 18:19:54 -0300

    UPDATE: Cap 7 - Pagina de Detalhamento do Produto

--- src/main/java/org/livraria/mb/BookDetailBean.java
+++ src/main/java/org/livraria/mb/BookDetailBean.java
@@ -1,29 +1,42 @@
 package org.livraria.mb;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.livraria.domain.dao.BookDAO;
 import org.livraria.domain.entity.Book;
 
 @Model
-public class ProductDetailBean {
+public class BookDetailBean {
 	
 	private BookDAO dao;
 	private Book book;
 	private Long id;
+	private HttpServletRequest request;
 	
-	public ProductDetailBean() {}
+	public BookDetailBean() {}
 	
 	@Inject
-	public ProductDetailBean(BookDAO dao) {
+	public BookDetailBean(BookDAO dao, HttpServletRequest request) {
 		this.dao = dao;
+		this.request = request;
+	}
+	
+	@PostConstruct
+	public void loadObjects() {
+		System.out.print("test");
+		System.out.println(request.getParameter("id"));
 	}
 	
 	public void loadBook() {
 		book = dao.findById(id);
 	}
 	
+	/*
+	 * Getters e Setters
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
